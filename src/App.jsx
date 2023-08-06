04d7912e731538342705e642b599ed9b63071418
Ferdinand Estoque <festoqufx@gmail.com>
2023-08-06 13:42:05 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import "./styles.css";
+import React from "react";
+import Slider from "react-slick";
+
+var settings = {
+  dots: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <Slider {...settings}>
+        <div className="card">1</div>
+        <div className="card">2</div>
+        <div className="card">3</div>
+        <div className="card">4</div>
+        <div className="card">5</div>
+        <div className="card">6</div>
+      </Slider>
+    </div>
+  );
+}
